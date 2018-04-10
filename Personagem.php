3d643ba049527fe8db4306494e07b05379ec5347
Henrique Rodrigues <henrique@hostnet.com.br>
2018-04-10 12:43:49 -0300

    Adicionando atributos

--- Personagem.php
+++ Personagem.php
@@ -4,5 +4,26 @@ namespace POO;
 
 class Personagem
 {
-    
+    private $nome;
+    private $cor;
+    private $quantidadeDeCogumelos;
+    private $tipoFisico;
+    private $possuiBigode;
+
+    # Forçando tipos nos atributos no construtor
+    public function __consruct(
+        string $nome,
+        string $cor,
+        int $quantidadeDeCogumelos,
+        float $altura,
+        string $tipoFisico,
+        bool $possuiBigode
+    ) {
+        $this->nome = $nome;
+        $this->cor = $cor;
+        $this->quantidadeDeCogumelos = $quantidadeDeCogumelos;
+        $this->altura = $altura;
+        $this->tipoFisico = $tipoFisico;
+        $this->possuiBigode = $possuiBigode;
+    }
 }
