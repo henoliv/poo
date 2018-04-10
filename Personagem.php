0bb40126526ec479650c4ccfda569dd7c6609bb3
Henrique Rodrigues <henrique@hostnet.com.br>
2018-04-10 13:17:10 -0300

    Adicinando métodos

--- Personagem.php
+++ Personagem.php
@@ -26,4 +26,79 @@ class Personagem
         $this->tipoFisico = $tipoFisico;
         $this->possuiBigode = $possuiBigode;
     }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getCor(): string
+    {
+        return $this->cor;
+    }
+
+    public function setCor($cor): void
+    {
+        $this->cor = $cor;
+    }
+
+    public function getQuantidadeDeCogumelos(): int
+    {
+        return $this->quantidadeDeCogumelos;
+    }
+
+    public function setQuantidadeDeCogumelos($quantidadeDeCogumelos): void
+    {
+        $this->quantidadeDeCogumelos = $quantidadeDeCogumelos;
+    }
+
+    public function getAltura(): float
+    {
+        return $this->altura;
+    }
+
+    public function setAltura($altura): void
+    {
+        $this->altura = $altura;
+    }
+
+    public function getTipoFisico(): string
+    {
+        return $this->tipoFisico;
+    }
+
+    public function setTipoFisico($tipoFisico): void
+    {
+        $this->tipoFisico = $tipoFisico;
+    }
+
+    public function getPossuiBigode(): bool
+    {
+        return $this->possuiBigode;
+    }
+
+    public function setPossuiBigode($possuiBigode): void
+    {
+        $this->possuiBigode = $possuiBigode;
+    }
+
+    public function pular(): void
+    {
+        // implementação
+    }
+
+    public function pegarCogumelo(Cogumelo $cogumelo): void
+    {
+        // implementação
+    }
+
+    public function atirarFogo(): BolaFogo
+    {
+        // implementação
+    }
 }
