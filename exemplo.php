fa650ee476e5acaa7477a9aa342e2344babf83cb
Henrique Rodrigues <henrique@hostnet.com.br>
2018-04-11 19:09:53 -0300

    Migrando testes para o PHPUnit

--- exemplo.php
+++ exemplo.php
@@ -2,27 +2,11 @@
 
 include_once('vendor/autoload.php');
 
-// use POO\Personagem;
 use POO\Hospital\Pessoa;
 use POO\Hospital\Paciente;
 use POO\Hospital\Funcionario;
 use POO\Hospital\Medico;
 use POO\Hospital\Gerente;
-
-// Teste de identidade e igualdade
-// $personagem = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
-// $personagem2 = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
-// var_dump($personagem->identical($personagem2));
-// var_dump($personagem->equals($personagem2));
-
-// Teste de representação
-// $personagem = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
-
-// var_dump($personagem->toString());
-
-// Teste de atributos e métodos estáticos
-// var_dump(Personagem::$quantidadeOlhos);
-// var_dump(Personagem::andar());
 
 // Testes com herança
 
---
+++ tests/PersonagemTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace POO;
+
+use POO\Personagem;
+use PHPUnit\Framework\TestCase;
+
+class PersonagemTest extends TestCase
+{
+    public function testIgualdade(): void
+    {
+        $personagem = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
+        $personagem2 = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
+
+        $this->assertTrue($personagem->equals($personagem2));
+    }
+
+    public function testIdentidade(): void
+    {
+        $personagem = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
+        $personagem2 = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
+
+        $this->assertFalse($personagem->identical($personagem2));
+    }
+
+    public function testRepresentacao(): void
+    {
+        $personagem = new Personagem('Mario', 'vermelho', 1, 1.6, 'gordo', true);
+
+        $this->assertEquals(
+            "Nome do personagem: Mario",
+            $personagem->toString()
+        );
+    }
+
+    public function testAtributoEstatico(): void
+    {
+        $this->assertEquals(2, Personagem::$quantidadeOlhos);
+    }
+
+    public function testMetodoEstatico()
+    {
+        $this->assertEquals(null, Personagem::andar());
+    }
+}
